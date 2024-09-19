17db4ca67868e440a60635cd2993e1db9ebd3d06
ronbb <ronbiaobiao@gmail.com>
2024-09-19 14:42:08 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div className="flex flex-row justify-center">
+      <div className="w-5xl">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
