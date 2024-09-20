bb9d5fce540cb23904d4e40f34e2d39d9515d74e
ronbb <ronbiaobiao@gmail.com>
2024-09-20 17:10:55 +0800

    feat: use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "@unocss/reset/tailwind.css";
 import "virtual:uno.css";
@@ -8,8 +8,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
