494d4954665aece76f93726e15cbd6bf1f97f3ac
Ronbb <ronbiaobiao@gmail.com>
2024-09-22 22:23:47 +0800

    feat: improve ui, close #19, close #20

--- src/Portfolios/Rebrella.tsx
+++ src/Portfolios/Rebrella.tsx
@@ -3,12 +3,12 @@ export const Rebrella = () => {
     <div className="flex flex-col gap-3">
       <img src="/images/rebrella/1.png" />
       <img src="/images/rebrella/2.png" />
+      <video src="/images/rebrella/rebrella.mp4" controls />
       <img src="/images/rebrella/3.png" />
       <img src="/images/rebrella/4.png" />
       <img src="/images/rebrella/5.png" />
       <img src="/images/rebrella/6.png" />
       <img src="/images/rebrella/7.png" />
-      <video src="/images/rebrella/rebrella.mp4" controls />
     </div>
   );
 };
