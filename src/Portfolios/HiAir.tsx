735162b1c590f3f5c54b27281628b73f2f20ffcb
Ronbb <ronbiaobiao@gmail.com>
2024-09-22 23:00:26 +0800

    feat: improve ui, close #21

--- src/Portfolios/HiAir.tsx
+++ src/Portfolios/HiAir.tsx
@@ -4,6 +4,7 @@ export const HiAir = () => {
       <img src="/images/hi_air/1.png" />
       <img src="/images/hi_air/2.png" />
       <img src="/images/hi_air/3.png" />
+      <video src="/images/hi_air/hiair.mp4" controls />
       <img src="/images/hi_air/4.png" />
       <img src="/images/hi_air/5.png" />
       <img src="/images/hi_air/6.png" />
